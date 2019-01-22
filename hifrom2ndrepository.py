9c1f60cf8bce1552136dadad8edb15999ffeb8f4
patroclica <popovicm@sheridancollege.ca>
2019-01-21 22:01:29 -0500

    Added  few modifications to the files

--- hifrom2ndrepository.py
+++ hifrom2ndrepository.py
@@ -1,7 +1,7 @@
 """
 hifrom2ndrepository that salutes you
 """
-if __name__ == '__magic__':
+if __name__ != '__magic__':
     print('Hi, Git world!')
 else:
     print('There is something wrong! Please review the code!')
